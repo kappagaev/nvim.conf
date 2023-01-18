d1337482b48f3474c6e1445fdaca78a7c1ca71c1
kkpagaev <kkpagaev@gmail.com>
2023-01-18 14:06:06 +0200

    c

--- lua/kkpagaev/remap.lua
+++ lua/kkpagaev/remap.lua
@@ -1,14 +1,14 @@
 local g = vim.g
 
 local function map(m, k, v)
-    vim.keymap.set(m, k, v, { silent = true })
+	vim.keymap.set(m, k, v, { silent = true })
 end
 
-g.mapleader=" "
-g.maplocalleader=" "
+g.mapleader = " "
+g.maplocalleader = " "
 
 map('n', "<leader>", "")
-map('n', '<C-j>', '4j') 
+map('n', '<C-j>', '4j')
 map('n', '<leader>d', 'Vyp')
 
 map('n', '<c-k>', '4k')
@@ -18,4 +18,3 @@ map('n', '<c-g>', 'gcc')
 map('n', '<leader>w', ':update<CR>')
 map('i', 'jj', '<Esc>')
 map('i', 'JJ', '<Esc>')
-
