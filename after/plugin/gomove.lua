535217a77b77f3ce9db817e14678d53ab7d83c62
kkpagaev <kkpagaev@gmail.com>
2023-02-11 16:34:20 +0200

    c

--- after/plugin/gomove.lua
+++ after/plugin/gomove.lua
@@ -1,9 +1,10 @@
 require("gomove").setup {
+  map_defaults = false
 }
 local map = vim.api.nvim_set_keymap
 
-map( "x", "<C-h>", "<Plug>GoVSMLeft", {} )
-map( "x", "<C-j>", "<Plug>GoVSMDown", {} )
-map( "x", "<C-k>", "<Plug>GoVSMUp", {} )
-map( "x", "<C-l>", "<Plug>GoVSMRight", {} )
+map( "x", "<A-h>", "<Plug>GoVSMLeft", {} )
+map( "x", "<A-j>", "<Plug>GoVSMDown", {} )
+map( "x", "<A-k>", "<Plug>GoVSMUp", {} )
+map( "x", "<A-l>", "<Plug>GoVSMRight", {} )
 
