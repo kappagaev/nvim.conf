cfc100b842d79d6925f0177340f4ff17b4dba55e
kkpagaev <kkpagaev@gmail.com>
2023-06-10 04:11:03 +0300

    c

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,11 +3,11 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   lazy = false,
   keys = {
-    { "<leader><space>", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
-    { "<leader>u",       "<cmd>Telescope find_files<CR>", desc = "Find Files" },
-    { "<leader>/",       "<cmd>Telescope live_grep<CR>",  desc = "Find a string" },
-    { "<leader>fh",      "<cmd>Telescope help_tags<CR>",  desc = "Help" },
-    { "<leader>fg",      "<cmd>Telescope git_status<CR>", desc = "Find Git" },
+    { "<leader><space>" },
+    { "<leader>u" },
+    { "<leader>/",      "<cmd>Telescope live_grep<CR>",  desc = "Find a string", silent = true },
+    { "<leader>fh",     "<cmd>Telescope help_tags<CR>",  desc = "Help",          silent = true },
+    { "<leader>fg",     "<cmd>Telescope git_status<CR>", desc = "Find Git",      silent = true },
   },
   config = function()
     local status_ok, telescope = pcall(require, "telescope")
@@ -88,6 +88,17 @@ return {
           preview_width = 0.6
         }
       }
+      -- { "<leader><space>", "<cmd>Telescope find_files<CR>", desc = "Find Files", silent = true },
+      -- { "<leader>u",       "<cmd>Telescope find_files<CR>", desc = "Find Files", silent = true },
+      -- { "<leader>/",       "<cmd>Telescope live_grep<CR>",  desc = "Find a string", silent = true },
+      -- { "<leader>fh",      "<cmd>Telescope help_tags<CR>",  desc = "Help", silent = true },
+      -- { "<leader>fg",      "<cmd>Telescope git_status<CR>", desc = "Find Git", silent = true },
     }
+    vim.keymap.set("n", "<leader><space>", require("telescope.builtin").find_files, {
+      silent = true
+    })
+    vim.keymap.set("n", "<leader>u", require("telescope.builtin").find_files, {
+      silent = true
+    })
   end
 }
